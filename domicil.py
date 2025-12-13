ccaf5c32dd9a079542975cbd3b66e646f496fa69
22awaisjutt-crypto <22awaisjutt@gmail.com>
2025-12-13 11:11:20 -0800

    create domicile

---
+++ domicil.py
@@ -0,0 +1,10 @@
+def cheak_domicile(age,domicile):
+    print("yours age and domicile")
+    if age>=18 and domicile=='lahore':
+        print("you are eligibilty for vote")
+    else:    
+        print("you are not eligibilty for vote")
+    
+ 
+ cheak_domicile()
+    
